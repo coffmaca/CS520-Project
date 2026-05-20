827bdce1fe5b5128c2c7e9d2bde373a739efe224
coffmaca <coffmaca@oregonstate.edu>
2026-05-20 15:49:03 -0600

    Updated preprocess.py to add 'Recipients' and 'Recipient_Count' columns to Dataframe.  These tally the unique recipients across To, Cc, and Bcc columns.

--- preprocess.py
+++ preprocess.py
@@ -12,6 +12,7 @@ def get_text_from_email(msg):
             parts.append( part.get_payload() )
     return ''.join(parts)
 
+
 def split_email_addresses(line):
     """To separate multiple email addresses"""
     if line and isinstance(line, str):
@@ -21,9 +22,27 @@ def split_email_addresses(line):
         addrs = None
     return addrs
 
-def main():
+
+def compile_unique_recipients(line):
+    """To compile unique recipients"""
+    if line and isinstance(line, list):
+        return line
+    else:
+        return []
+
+
+def count_recipients(line):
+    """To count and record the number of recipients"""
+    count = 0
+    if line and isinstance(line, list):
+        count += len(line)
+
+    return count
+
+
+def ingest_csv(file_path):
     # Load emails
-    emails_df = pd.read_csv('emails.csv')
+    emails_df = pd.read_csv(file_path)
 
     # Parse the emails into a list email objects
     messages = list(map(email.message_from_string, emails_df['message']))
@@ -44,6 +63,15 @@ def main():
     # Split multiple email addresses
     emails_df['From'] = emails_df['From'].map(split_email_addresses)
     emails_df['To'] = emails_df['To'].map(split_email_addresses)
+    emails_df['Cc'] = emails_df['Cc'].map(split_email_addresses)
+    emails_df['Bcc'] = emails_df['Bcc'].map(split_email_addresses)
+
+    # Tally unique recipients
+    emails_df["Recipients"] = emails_df[["To", "Cc", "Bcc"]].apply(
+        lambda row: list(set().union(*map(compile_unique_recipients, row))),
+        axis=1
+    )
+    emails_df["Recipient_Count"] = emails_df["Recipients"].map(len)
 
     # Extract the root of 'file' as 'user'
     emails_df['user'] = emails_df['file'].map(lambda x: x.split('/')[0])
@@ -68,8 +96,23 @@ def main():
     ], axis=1)
 
     # Parse datetime
-    emails_df['Date'] = pd.to_datetime(emails_df['Date'], utc=True) #, infer_datetime_format=True)
-    emails_df.to_pickle('emails_preprocessed2.pkl')
+    emails_df['Date'] = pd.to_datetime(emails_df['Date'], utc=True)  # , infer_datetime_format=True)
+
+    return emails_df
+
+
+def main(file_path: str, save_df: bool = True):
+    if file_path == "data/emails.csv":
+        emails_df = ingest_csv(file_path)
+        if save_df:
+            emails_df.to_pickle('data/emails_preprocessed.pkl')
+    else:
+        emails_df = pd.read_pickle(file_path)
+
+    print("")
+
 
 if __name__ == '__main__':
-    main()
+    emails_path = "data/emails.csv"
+    emails_df_path = "data/emails_preprocessed.pkl"
+    main(emails_path)
